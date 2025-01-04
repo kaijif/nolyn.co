b2a70498a2824a1e57054d5405285682aaa49fe3
kaijif <thefuka2@gmail.com>
2025-01-04 18:07:31 -0500

    remove testimonials

--- components/Columns/Columns.js
+++ components/Columns/Columns.js
@@ -33,42 +33,42 @@ const ColumnData = [
 
 export const Columns = () => {
     return (
-        <SectionContainer className="benefits-lists grid gap-x-8 gap-y-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-16">
-            {ColumnData.map((item) => (
-                <div
-                    id={item.id}
-                    key={item.id}
-                    className="benefits-list--item text-[#737373] text-left"
-                >
-                    <Icon icon={item.icon} className="mb-4 w-10 h-10 my-2" />
-                    <h3 className="text-xl mb-2 font-medium text-black">
-                        {item.title}
-                    </h3>
-                    <p>{item.content}</p>
-                    <o className="flex">
-                        <Icon
-                            icon="solar:star-bold"
-                            className="h-10 mr-1 text-secondary-500"
-                        />
-                        <Icon
-                            icon="solar:star-bold"
-                            className="h-10 mr-1 text-secondary-500"
-                        />
-                        <Icon
-                            icon="solar:star-bold"
-                            className="h-10 mr-1 text-secondary-500"
-                        />
-                        <Icon
-                            icon="solar:star-bold"
-                            className="h-10 mr-1 text-secondary-500"
-                        />
-                        <Icon
-                            icon="solar:star-bold"
-                            className="h-10 mr-1 text-secondary-500"
-                        />
-                    </o>
-                </div>
-            ))}
-        </SectionContainer>
+        // <SectionContainer className="benefits-lists grid gap-x-8 gap-y-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-16">
+        //     {ColumnData.map((item) => (
+        //         <div
+        //             id={item.id}
+        //             key={item.id}
+        //             className="benefits-list--item text-[#737373] text-left"
+        //         >
+        //             <Icon icon={item.icon} className="mb-4 w-10 h-10 my-2" />
+        //             <h3 className="text-xl mb-2 font-medium text-black">
+        //                 {item.title}
+        //             </h3>
+        //             <p>{item.content}</p>
+        //             <o className="flex">
+        //                 <Icon
+        //                     icon="solar:star-bold"
+        //                     className="h-10 mr-1 text-secondary-500"
+        //                 />
+        //                 <Icon
+        //                     icon="solar:star-bold"
+        //                     className="h-10 mr-1 text-secondary-500"
+        //                 />
+        //                 <Icon
+        //                     icon="solar:star-bold"
+        //                     className="h-10 mr-1 text-secondary-500"
+        //                 />
+        //                 <Icon
+        //                     icon="solar:star-bold"
+        //                     className="h-10 mr-1 text-secondary-500"
+        //                 />
+        //                 <Icon
+        //                     icon="solar:star-bold"
+        //                     className="h-10 mr-1 text-secondary-500"
+        //                 />
+        //             </o>
+        //         </div>
+        //     ))}
+        // </SectionContainer>
     );
 };
